1c1aca7b0df0bfde7e66ea64250dd39c9e4ab89b
Marcel Link <marcel.link@datev.de>
2026-02-12 16:49:28 +0100

    update project structure

--- TF/src/main/java/org/lets_do_this_the_easy_way/House.java
+++
@@ -1,8 +0,0 @@
-package org.lets_do_this_the_easy_way;
-
-import org.lets_do_this_the_easy_way.annotations.ToMultiSourceDTO;
-
-@ToMultiSourceDTO(identifierName = "United")
-public class House {
-    String address;
-}
--- TF/src/main/java/org/lets_do_this_the_easy_way/Sergey.java
+++
@@ -1,8 +0,0 @@
-package org.lets_do_this_the_easy_way;
-
-import org.lets_do_this_the_easy_way.annotations.ToMultiSourceDTO;
-
-@ToMultiSourceDTO(identifierName = "SuperSergey")
-public class Sergey {
-    int age;
-}
